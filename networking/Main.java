2dc9749bef49ac47cea349edbb4ecb69feacbb66
thandoncube1 <ncubethando20@gmail.com>
2025-07-03 18:28:54 -0400

    Wrote the main class code

---
+++ networking/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+final int PORT_NUMBER = 3001;
+
+void main() {
+    try (var scanner = new Scanner(System.in)) {
+        System.out.println("Is this a server? (Y/N)");
+        if (scanner.nextLine().equalsIgnoreCase("Y")) {
+            new MyServer().start(PORT_NUMBER);
+        } else {
+            System.out.println("Client TODO");
+        }
+    }
+}
